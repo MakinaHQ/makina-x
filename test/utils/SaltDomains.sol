259af33e47ac9a830bae8c2b2ffd51d23e491cdc
clm6671 <33152614+clm6671@users.noreply.github.com>
2026-07-16 17:58:37 +0200

    chore: update salt domains

--- test/utils/SaltDomains.sol
+++ test/utils/SaltDomains.sol
@@ -2,14 +2,14 @@
 pragma solidity 0.8.35;
 
 abstract contract SaltDomains {
-    bytes32 internal constant MAKINA_X_REGISTRY_SALT_DOMAIN = keccak256("makina.salt.MakinaXRegistry");
+    bytes32 internal constant MAKINA_X_REGISTRY_SALT_DOMAIN = keccak256("makinax.salt.MakinaXRegistry");
 
-    bytes32 internal constant MODULE_FACTORY_SALT_DOMAIN = keccak256("makina.salt.ModuleFactory");
+    bytes32 internal constant MODULE_FACTORY_SALT_DOMAIN = keccak256("makinax.salt.ModuleFactory");
 
-    bytes32 internal constant ACROSS_V4_BRIDGE_ENCODER_SALT_DOMAIN = keccak256("makina.salt.AcrossV4BridgeEncoder");
+    bytes32 internal constant ACROSS_V4_BRIDGE_ENCODER_SALT_DOMAIN = keccak256("makinax.salt.AcrossV4BridgeEncoder");
 
     bytes32 internal constant LAYER_ZERO_V2_BRIDGE_ENCODER_SALT_DOMAIN =
-        keccak256("makina.salt.LayerZeroV2BridgeEncoder");
+        keccak256("makinax.salt.LayerZeroV2BridgeEncoder");
 
-    bytes32 internal constant CCTP_V2_BRIDGE_ENCODER_SALT_DOMAIN = keccak256("makina.salt.CctpV2BridgeEncoder");
+    bytes32 internal constant CCTP_V2_BRIDGE_ENCODER_SALT_DOMAIN = keccak256("makinax.salt.CctpV2BridgeEncoder");
 }
