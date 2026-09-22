72ee5b4e58807f6e58bb692e4c6f607c11cc7c39
clm6671 <33152614+clm6671@users.noreply.github.com>
2026-09-22 17:54:00 +0200

    chore: bump solhint to 6.2.4 + scope rules per directory

--- script/deploy/utils/CreateModuleBase.sol
+++ script/deploy/utils/CreateModuleBase.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.35;
 
-// console logging is used to expose the call's calldata in view mode
-// solhint-disable no-console
-
 import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
 
